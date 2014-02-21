6a4529c13f8303eed2809fffa81229eac15f4fbc
Christoffer Winterkvist <chris@zenangst.com>
2014-02-21 17:39:17 +0100

    Added HYPPlugInsController

---
+++ Hyper Active/HYPPlugInsController.h
@@ -0,0 +1,16 @@
+//
+//  HYPPlugInsController.h
+//  Hyper Active
+//
+//  Created by Christoffer Winterkvist on 2/21/14.
+//  Copyright (c) 2014 Christoffer Winterkvist. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface HYPPlugInsController : NSObject
+
+- (NSString *)hyperActiveVersion;
+- (NSUInteger)apiVersion;
+
+@end
---
+++ Hyper Active/HYPPlugInsController.m
@@ -0,0 +1,22 @@
+//
+//  HYPPlugInsController.m
+//  Hyper Active
+//
+//  Created by Christoffer Winterkvist on 2/21/14.
+//  Copyright (c) 2014 Christoffer Winterkvist. All rights reserved.
+//
+
+#import "HYPPlugInsController.h"
+
+@implementation HYPPlugInsController
+
+- (NSString *)hyperActiveVersion
+{
+	return [[[NSBundle bundleForClass:[self class]] infoDictionary] objectForKey:@"CFBundleVersion"];
+}
+- (NSUInteger)apiVersion
+{
+	return 1.0;
+}
+
+@end
