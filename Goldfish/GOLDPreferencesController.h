f01eda03eab32f68e2c3a2977e0e702b3173dda1
Christoffer Winterkvist <christoffer@winterkvist.com>
2014-04-14 19:56:30 +0200

    Add new preferences controller

---
+++ Goldfish/GOLDPreferencesController.h
@@ -0,0 +1,13 @@
+//
+//  GOLDPreferencesController.h
+//  Goldfish
+//
+//  Created by Christoffer Winterkvist on 14/04/14.
+//  Copyright (c) 2014 zenangst. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface GOLDPreferencesController : NSWindowController
+
+@end
---
+++ Goldfish/GOLDPreferencesController.m
@@ -0,0 +1,34 @@
+//
+//  GOLDPreferencesController.m
+//  Goldfish
+//
+//  Created by Christoffer Winterkvist on 14/04/14.
+//  Copyright (c) 2014 zenangst. All rights reserved.
+//
+
+#import "GOLDPreferencesController.h"
+
+@interface GOLDPreferencesController ()
+
+@end
+
+@implementation GOLDPreferencesController
+
+- (id)initWithWindow:(NSWindow *)window
+{
+    self = [super initWithWindow:window];
+    if (self) {
+        [self setWindowFrameAutosaveName:@"PreferencesWindow"];
+        [self loadWindow];
+    }
+    return self;
+}
+
+- (void)windowDidLoad
+{
+    [super windowDidLoad];
+
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
