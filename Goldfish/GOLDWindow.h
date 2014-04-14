466a4b0ef9e6e88b19520cfa55ae3323338ff13f
Christoffer Winterkvist <christoffer@winterkvist.com>
2014-04-14 19:56:14 +0200

    Add new GOLDWindow implementation

---
+++ Goldfish/GOLDWindow.h
@@ -0,0 +1,15 @@
+//
+//  GOLDWindow.h
+//  Goldfish
+//
+//  Created by Christoffer Winterkvist on 14/04/14.
+//  Copyright (c) 2014 zenangst. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface GOLDWindow : NSWindow
+
+- (instancetype)initWithContentRect:(NSRect)rect;
+
+@end
---
+++ Goldfish/GOLDWindow.m
@@ -0,0 +1,23 @@
+//
+//  GOLDWindow.m
+//  Goldfish
+//
+//  Created by Christoffer Winterkvist on 14/04/14.
+//  Copyright (c) 2014 zenangst. All rights reserved.
+//
+
+#import "GOLDWindow.h"
+
+@implementation GOLDWindow
+
+- (instancetype)initWithContentRect:(NSRect)rect
+{
+    self = [super initWithContentRect:rect styleMask:NSTitledWindowMask|NSResizableWindowMask|NSMiniaturizableWindowMask|NSClosableWindowMask backing:NSBackingStoreBuffered defer:NO];
+    if (self) {
+        [self setHasShadow:YES];
+        [[self contentView] setAutoresizesSubviews:YES];
+    }
+    return self;
+}
+
+@end
