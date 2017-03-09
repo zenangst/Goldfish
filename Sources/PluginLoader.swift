1614bda5d6ea3a52fc72d5e6e3fdf7c55ebd8e5a
Christoffer Winterkvist <christoffer@winterkvist.com>
2017-03-10 00:13:49 +0100

    Implement base plugin loader

---
+++ Sources/PluginLoader.swift
@@ -0,0 +1,33 @@
+import Foundation
+
+enum PluginError: Error {
+  case failedToLoad
+  case typeCastingFailed(String)
+  case conformFailed
+}
+
+class PluginLoader {
+
+  var plugins: [Plugin] = []
+
+  func load(plugin: String, at path: Path) throws {
+    let pluginPath = path.rawValue
+      .appending("/")
+      .appending(plugin)
+
+    guard let bundle = Bundle.init(path: pluginPath), bundle.load() else {
+      throw PluginError.failedToLoad
+    }
+
+    guard let pluginClass = bundle.principalClass as? NSObject.Type else {
+      throw PluginError.typeCastingFailed("Plugin principal class does not inherit from NSObject")
+    }
+
+    guard let plugin = pluginClass.init() as? Plugin else {
+      throw PluginError.conformFailed
+    }
+
+    plugins.append(plugin)
+  }
+
+}
