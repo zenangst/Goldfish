57296ef4e3a87a3645d793af380028dc52d25054
Christoffer Winterkvist <christoffer@winterkvist.com>
2017-03-09 19:13:24 +0100

    Remove boilerplate from AppDelegate

--- Goldfish/AppDelegate.swift
+++ Goldfish/AppDelegate.swift
@@ -1,27 +1,10 @@
-//
-//  AppDelegate.swift
-//  Goldfish
-//
-//  Created by Christoffer Winterkvist on 3/9/17.
-//  Copyright © 2017 Christoffer Winterkvist. All rights reserved.
-//
-
 import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-  @IBOutlet weak var window: NSWindow!
-
-
   func applicationDidFinishLaunching(_ aNotification: Notification) {
-    // Insert code here to initialize your application
+
   }
-
-  func applicationWillTerminate(_ aNotification: Notification) {
-    // Insert code here to tear down your application
-  }
-
-
 }
 
