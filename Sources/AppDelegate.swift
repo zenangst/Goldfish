a6adfda21c85183ca76cce41f398dde450630b12
Christoffer Winterkvist <christoffer@winterkvist.com>
2017-03-10 12:59:45 +0100

    Refactor AppDelegate to handle windows

--- Sources/AppDelegate.swift
+++ Sources/AppDelegate.swift
@@ -6,6 +6,9 @@ let pluginLoader = PluginLoader()
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
+  static let windowStyleMask: NSWindowStyleMask = [.titled, .resizable]
+  var window: NSWindow?
+  var tabs: [NSWindow] = []
   var directoryWather: DirectoryWatcher?
 
   func applicationDidFinishLaunching(_ aNotification: Notification) {
@@ -33,10 +36,33 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
   func didFinishLoadingPlugins() {
     directoryWather = DirectoryWatcher(path: .documents, pluginsDidChange)
+
+    for container in pluginLoader.pluginContainers {
+      let window = NSWindow(contentRect: NSRect.zero,
+                            styleMask: AppDelegate.windowStyleMask,
+                            backing: .retained, defer: false)
+      container.plugin.pluginDidLoad()
+      window.title = container.plugin.name
+      window.contentViewController = container.plugin.controller
+      let size = CGSize(width: 640, height: 320)
+      window.minSize = size
+      window.setFrame(NSRect(origin: CGPoint.zero, size: size), display: false)
+
+      if self.window == nil {
+        self.window = window
+        window.center()
+        window.makeKeyAndOrderFront(nil)
+      }
+
+      self.window?.addTabbedWindow(window, ordered: .below)
+      self.tabs.append(window)
+    }
   }
 
   func pluginsDidChange(watcher: DirectoryWatcher) {
-    /// Implement reloading bundles.
+//    for container in pluginLoader.pluginContainers {
+////      container.plugin.pluginDidLoad()
+//    }
   }
 }
 
